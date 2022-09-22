6f91024f218e9c890aeb521e827dba47470de76a
Limjongho <whdgh9873@icloud.com>
2022-09-22 17:49:41 +0900

    모던 자바 인 액션 : 5장 스트림의 활용 ~ flatMap

--- src/menu/DishPractice.java
+++ src/menu/DishPractice.java
@@ -5,19 +5,18 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class DishPractice {
-    public static void main(String[] args) {
-        List<Dish> menu = Arrays.asList(
-                new Dish("pork", false, 800, Type.MEAT),
-                new Dish("beef", false, 700, Type.MEAT),
-                new Dish("chicken", false, 400, Type.MEAT),
-                new Dish("french fries", true, 530, Type.OTHER),
-                new Dish("rice", true, 350, Type.OTHER),
-                new Dish("season fruit", true, 120, Type.OTHER),
-                new Dish("pizza", true, 550, Type.OTHER),
-                new Dish("prawns", false, 300, Type.FISH),
-                new Dish("salmon", false, 450, Type.FISH)
-        );
+    public static List<Dish> menu = Arrays.asList(
+            new Dish("pork", false, 800, Type.MEAT),
+            new Dish("beef", false, 700, Type.MEAT),
+            new Dish("chicken", false, 400, Type.MEAT),
+            new Dish("french fries", true, 530, Type.OTHER),
+            new Dish("rice", true, 350, Type.OTHER),
+            new Dish("season fruit", true, 120, Type.OTHER),
+            new Dish("pizza", true, 550, Type.OTHER),
+            new Dish("prawns", false, 300, Type.FISH),
+            new Dish("salmon", false, 450, Type.FISH));
 
+    public static void main(String[] args) {
         /**
          * 메뉴 stream 에서 칼로리 300 이상인 음식의 이름을 3개까지 추출하는 예제
          * 데이터 소스 : menu
---
+++ src/menu/UsingStream.java
@@ -0,0 +1,192 @@
+package menu;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class UsingStream {
+    public static void main(String[] args) {
+        List<Dish> list = DishPractice.menu;
+
+        /**
+         * filtering 연습
+         * 소스 : list
+         * 요소 : Dish
+         * 중간 연산 : filter ( 프레디케이트 함수를 파라미터로 받아서 프레디케이트와 일치하는 요소를 포함하는 Stream 을 반환하는 연산 )
+         * 최종 연산 : collect
+         */
+        List<Dish> collect = list.stream().filter(
+                Dish::isVegetarian
+        ).collect(Collectors.toList());
+
+        /**
+         * filtering 연습
+         * 소스 : intList
+         * 요소 : Integer
+         * 중간 연산 : filter -> distinct() (고유 요소만 필터링 할 수 있도록 하는 연산)
+         * 최종 연산 : forEach
+         */
+        List<Integer> intList = Arrays.asList(1, 2, 7, 5, 9, 10, 2, 7, 6);
+        intList.stream().filter(a -> a % 2 == 0)
+                .distinct()
+                .forEach(System.out::println);
+
+
+        /**
+         * Stream 슬라이싱
+         * 소스 : list
+         * 요소 : Dish
+         * 중간 연산 : takeWhile (Predicate<T> 를 파라미터로 갖는 연산으로 최초 false 가 되는 지점 직전가지의 값들만 추출한다.)
+         * 최종 연산 : forEach
+         */
+        list.stream()
+                .takeWhile(a -> a.getCalories() > 320)
+                .forEach(System.out::println);
+
+        /**
+         * Stream 슬라이싱
+         * 소스 : list
+         * 요소 : Dish
+         * 중간 연산 : dropWhile (Predicate<T> 를 파라미터로 갖는 연산으로 최초 false 가 되는 지점 이후부터의 값들만 추출한다,
+         * 앞의 takeWhile 과 정반대의 연산을 수행한다.)
+         * 최종 연산 : forEach
+         */
+        list.stream()
+                .dropWhile(a -> a.getCalories() > 320)
+                .forEach(System.out::println);
+
+        /** Stream 축소
+         * 소스 : list
+         * 요소 : Dish
+         * 중간 연산 : filter -> limit
+         * 최종 연산 : collect
+         */
+        list.stream()
+                .filter(a -> a.getCalories() > 300)
+                .limit(3)
+                .collect(Collectors.toList());
+
+        /**
+         * Stream 요소 건너뛰기
+         * 소스 : list
+         * 요소 : Dish
+         * 중간 연산 : filter -> skip
+         * 최종연산 : forEach
+         */
+        System.out.println("요소 건너뛰기");
+        list.stream()
+                .filter(d -> d.getCalories() > 300)
+                .skip(2)
+                .forEach(System.out::println);
+
+        /**
+         * 예제 : 처음 등장하는 두 고기 요리를 필터링 하시오
+         * 소스 : list
+         * 요소 : Dish
+         * 중간 연선 : filter -> limit(2)
+         * 최종 연산 : forEach
+         */
+        System.out.println("에제");
+        list.stream()
+                .filter(d->d.getType().equals(Type.MEAT))
+                .limit(2)
+                .forEach(System.out::println);
+
+        /**
+         * mapping
+         * : 기존의 값을 고치는 것이 아닌, 새로운 버전을 만든다는 개념
+         * 함수를 인수로 받는 연산으로 각 요소에 함수 적용 결과 새로운 요소로 변환된다.
+         * 요소들의 요리명을 추출하는 연산
+         * 소스 : list
+         * 요소 : Dish
+         * 중간연선 : map
+         * 최종연산 : collect
+         */
+        List<String> collect1 = list.stream()
+                .map(Dish::getName)
+                .collect(Collectors.toList());
+        /**
+         * 추출한 요리명의 글자 수를 추출하는 연산
+         * 소스 : collect1
+         * 요소 : String
+         * 중간 연선 : map
+         * 최종 연산 : collect
+         */
+        List<Integer> collect2 = collect1.stream()
+                .map(String::length)
+                .collect(Collectors.toList());
+
+        /**
+         * 위의 두 과정 합치기
+         */
+        List<Integer> collect3 = list.stream()
+                .map(Dish::getName)
+                .map(String::length)
+                .collect(Collectors.toList());
+
+        /**
+         * 문자열 배열에서 고유 문자로 이루어진 List 를 반환하는 기능 실패 예제
+         *
+         */
+        String[] arr = {"Hello", "world"};
+        Arrays.stream(arr)
+                .map(s -> s.split(""))
+                .map(Arrays::stream)
+                .distinct()
+                .collect(Collectors.toList());
+
+        List<String> collect4 = Arrays.stream(arr)
+                .map(s -> s.split(""))
+                .flatMap(Arrays::stream)
+                .distinct()
+                .collect(Collectors.toList());
+
+        for (String s : collect4) {
+            System.out.println(s);
+        }
+
+        /**
+         * 숫자 리스트 -> 제곱 리스트 예제
+         * 소스 : {1,2,3,4,5}
+         * 요소 : int
+         * 중간 연산 : map
+         * 최종 연산 : toArray
+         */
+        int[] ints = {1, 2, 3, 4, 5};
+        int[] ints1 = Arrays.stream(ints)
+                .map(i -> i * i)
+                .toArray();
+        for (int i : ints1) {
+            System.out.println(i);
+        }
+
+        /**
+         * 두개의 숫자 리스트 -> 모든 숫자 2쌍의 리스트 반환
+         */
+        List<Integer> integerList = Arrays.asList(1, 2, 3);
+        List<Integer> integerList1 = Arrays.asList(4, 5);
+
+        List<int[]> collect5 = integerList.stream()
+                .flatMap(i -> integerList1.stream()
+                        .map(j -> new int[]{i, j}))
+                .collect(Collectors.toList());
+        for (int[] ints2 : collect5) {
+            System.out.println(ints2[0]);
+            System.out.println(ints2[1]);
+        }
+
+        /**
+         * 이전 예제에서 숫자 쌍의 합이 3의 배수인 것만 추출
+         */
+        List<int[]> collect6 = integerList.stream()
+                .flatMap(i -> integerList1.stream()
+                        .filter(j->(i+j)%3==0)
+                        .map(j -> new int[]{i, j}))
+                .collect(Collectors.toList());
+        for (int[] ints2 : collect6) {
+            System.out.println(ints2[0]);
+            System.out.println(ints2[1]);
+        }
+    }
+}
